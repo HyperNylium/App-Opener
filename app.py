d81574a2744efcb0f5b6764684f84f575c6ffb16
HyperNylium <davidpivik123@gmail.com>
2022-04-14 22:16:40 +0300

    main

---
+++ app.py
@@ -0,0 +1,255 @@
+###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+###
+### Author/Creator: HyperNylium
+###
+### Website: http://www.hypernylium.com
+###
+### GitHub:  https://github.com/HyperNylium/
+###
+### License:  Mozilla Public License Version 2.0
+###
+### IMPORTANT:  I OFFER NO WARRANTY OR GUARANTEE FOR THIS SCRIPT. USE AT YOUR OWN RISK.
+###             I tested it on my own and implemented some failsafes as best as I could,
+###             but there could always be some kind of bug. You should inspect the code yourself.
+###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+App_Version = "1.2.0"
+
+import time
+import platform
+import os
+import webbrowser
+from tkinter import *
+
+clear_command = "cls" if platform.system() == "Windows" else "clear"
+
+# Text Colors
+CLR_RED = "\033[31m"
+CLR_GREEN = "\033[32m"
+CLR_YELLOW = "\033[33m"
+CLR_BLUE = "\033[34m"
+CLR_CYAN = "\033[36m"
+CLR_WHİTE = "\033[37m"
+
+# Background Colors
+BG_RED = "\033[41m"
+BG_GREEN = "\033[42m"
+BG_YELLOW = "\033[43m"
+BG_BLUE = "\033[44m"
+BG_CYAN = "\033[46m"
+BG_WHİTE = "\033[47m"
+
+# Styles 
+BRİGHT = "\033[1m"
+RESET_ALL = "\033[0m"
+
+while True:
+  os.system(clear_command)
+  print(CLR_CYAN + "\n\n      Wellcome To App Launcher")
+  RESET_ALL
+  input(CLR_GREEN + "\n      Press 'Enter' to Launch App")
+  RESET_ALL
+
+  print(CLR_GREEN + "\n\n -")
+  time.sleep(0.1)
+
+  os.system(clear_command)
+  print(CLR_GREEN + "\n\n -- ")
+  time.sleep(0.1)
+
+  os.system(clear_command)
+  print(CLR_GREEN + "\n\n --- ")
+  time.sleep(0.1)
+
+  os.system(clear_command)
+  print(CLR_GREEN + "\n\n ---- ")
+  time.sleep(0.1)
+
+  os.system(clear_command)
+  print(CLR_GREEN + "\n\n -----")
+  time.sleep(0.1)
+
+  os.system(clear_command)
+  print(CLR_GREEN + "\n\n ------")
+  time.sleep(0.1)
+
+  os.system(clear_command)
+  print(CLR_GREEN + "\n\n -------")
+  time.sleep(0.1)
+
+  os.system(clear_command)
+  print(CLR_GREEN + "\n\n --------")
+  time.sleep(0.1)
+
+  os.system(clear_command)
+  print(CLR_GREEN + "\n\n ---------")
+  time.sleep(0.1)
+
+  os.system(clear_command)
+  print(CLR_GREEN + "\n\n ----------")
+  time.sleep(0.1)
+
+  os.system(clear_command)
+  print(CLR_GREEN + "\n\n -----------")
+  time.sleep(0.1)
+
+  os.system(clear_command)
+  print(CLR_GREEN + "\n\n ------------")
+  time.sleep(0.1)
+
+  os.system(clear_command)
+  print(CLR_GREEN + "\n\n -------------")
+  time.sleep(0.1)
+
+  os.system(clear_command)
+  print(CLR_GREEN + "\n\n --------------")
+  time.sleep(0.1)
+
+  os.system(clear_command)
+  print(CLR_GREEN + "\n\n ---------------")
+  time.sleep(0.1)
+
+  os.system(clear_command)
+  print(CLR_GREEN + "\n\n ----------------")
+  time.sleep(0.1)
+
+  os.system(clear_command)
+  print(CLR_GREEN + "\n\n -----------------")
+  time.sleep(0.1)
+
+  os.system(clear_command)
+  print(CLR_GREEN + "\n\n ------------------")
+  time.sleep(0.1)
+
+  os.system(clear_command)
+  print(CLR_GREEN + "\n\n -------------------")
+  time.sleep(0.1)
+
+  os.system(clear_command)
+  print(CLR_GREEN + "\n\n --------------------")
+  time.sleep(0.1)
+
+  os.system(clear_command)
+  print(CLR_GREEN + "\n\n ---------------------")
+  time.sleep(0.1)
+
+  os.system(clear_command)
+  print(CLR_GREEN + "\n\n ----------------------")
+  time.sleep(0.1)
+  RESET_ALL
+  os.system(clear_command)
+
+  print(CLR_CYAN + "\n\n    Files Are Getting Ready - \ ")
+  time.sleep(0.4)
+  os.system(clear_command)
+
+  print("\n\n    Files Are Getting Ready - | ")
+  time.sleep(0.4)
+  os.system(clear_command)
+  
+  print("\n\n    Files Are Getting Ready - / ")
+  time.sleep(0.4)
+  os.system(clear_command)
+  
+  print("\n\n    Files Are Getting Ready - | ")
+  time.sleep(0.4)
+  os.system(clear_command)
+  
+  print("\n\n    Files Are Getting Ready - \ ")
+  time.sleep(0.4)
+  os.system(clear_command)
+  
+  print("\n\n    Files Are Getting Ready - | ")
+  time.sleep(0.4)
+  os.system(clear_command)
+  
+  print("\n\n    Files Are Getting Ready - / ")
+  time.sleep(0.4)
+  os.system(clear_command)
+  
+  print("\n\n    Files Are Getting Ready - | ")
+  time.sleep(0.4)
+  os.system(clear_command)
+  
+  print("\n\n    Files Are Getting Ready - \ ")
+  time.sleep(0.4)
+  os.system(clear_command)
+
+  print("\n\n    importing files - \ ")
+  time.sleep(0.4)
+  os.system(clear_command)
+
+  print("\n\n    importing files - | ")
+  time.sleep(0.4)
+  os.system(clear_command)
+
+  print("\n\n    importing files - / ")
+  time.sleep(0.4)
+  os.system(clear_command)
+
+  print("\n\n    importing files - | ")
+  time.sleep(0.4)
+  os.system(clear_command)
+
+  print("\n\n    importing files - \ ")
+  time.sleep(0.4)
+  os.system(clear_command)
+
+  print("\n\n    importing files - | ")
+  time.sleep(0.4)
+  os.system(clear_command)
+
+  print("\n\n    importing files - / ")
+  time.sleep(0.4)
+  os.system(clear_command)
+
+  print("\n\n    importing files - | ")
+  time.sleep(0.4)
+  os.system(clear_command)
+
+  print("\n\n    imported all files ")
+  RESET_ALL
+
+  Link1 = "" # Put in what ever you want to open (file/folder/website/etc)
+  Link2 = "" # Put in what ever you want to open (file/folder/website/etc)
+  Link3 = "" # Put in what ever you want to open (file/folder/website/etc)
+
+  webbrowser.open(Link1)
+  time.sleep(1) # Time inbetween opening links. For now it is 1 second.
+  webbrowser.open(Link2)
+  time.sleep(1) # Time inbetween opening links. For now it is 1 second.
+  webbrowser.open(Link3)
+  time.sleep(6)
+  os.system(clear_command)
+
+  print(CLR_RED + "\n\n       Program Will Be Terminated in")
+  print(CLR_RED + "                    5")
+  time.sleep(1)
+  os.system(clear_command)
+
+  print(CLR_RED + "\n\n       Program Will Be Terminated in")
+  print(CLR_RED + "                    4")
+  time.sleep(1)
+  os.system(clear_command)
+
+  print(CLR_RED + "\n\n       Program Will Be Terminated in")
+  print(CLR_RED + "                    3")
+  time.sleep(1)
+  os.system(clear_command)
+
+  print(CLR_RED + "\n\n       Program Will Be Terminated in")
+  print(CLR_RED + "                    2")
+  time.sleep(1)
+  os.system(clear_command)
+
+  print(CLR_RED + "\n\n       Program Will Be Terminated in")
+  print(CLR_RED + "                    1")
+  time.sleep(1)
+  os.system(clear_command)
+
+  input(CLR_GREEN + "\n      Press 'Enter' to Exit...")
+  time.sleep(0.2)
+  input(CLR_YELLOW + "\n     Are You Sure You Want To Exit?")
+  print(CLR_RED + "\n             Terminating!")
+  time.sleep(2)
+  os.system(clear_command)
+  break
